8eaf566d84670a9890a8cb1818cf4fba68c63a42
Allaine Paredes <aparedes2@nait.ca>
2022-01-14 11:38:35 -0700

    jan14

---
+++ OOPDemo01/OOPDemo01/Course.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOPDemo01
+{
+    internal class Course
+    {
+        #region Readonly Data Fields
+        public readonly string CourseNo;
+        public readonly string CourseName;
+        public readonly List<string> Students = new List<string>();
+        #endregion
+
+        #region
+        public int StudentCount
+        {
+            get { return Students.Count; }
+        }
+        #endregion
+
+        #region Constructors
+        public Course(string courseNo, string courseName)
+        {
+            // Validate that courseNo is not null,
+            // or an empty string
+            // and must contain exactly 8 characters
+            // where the first 4 characters are letters
+            // and the last 4 characters are digits
+            if ( string.IsNullOrEmpty( courseNo ))
+            {
+                throw new ArgumentNullException("CourseNo is required.");
+            }
+            if ( courseNo.Length != 8)
+            {
+                throw new ArgumentException("CourseNo contain exactly 8 characters.");
+            }
+
+            CourseNo = courseNo;
+
+
+            // Validate that courseName is not null or an empty string
+            if ( string.IsNullOrEmpty( courseName))
+            {
+                throw new ArgumentException("CourseName is required.");
+            }
+            CourseName = courseName;
+        }
+        #endregion
+
+        #region
+        public void AddStudent(string name)
+        {
+            Students.Add(name);
+        }
+        public void RemoveStudent(string name)
+        {
+            Students.Remove(name);
+        }
+        public bool SaveToFile(String filePath)
+        {
+            bool success = false;
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // Write to the file the CourseNo and CourseName
+                    writer.WriteLine($"{CourseNo}");
+                    writer.WriteLine($"{CourseName}");
+                    // Write to the file the name of all the students in the course
+                    foreach (var student in Students)
+                    {
+                        writer.WriteLine(student);
+                    }
+                    writer.Close();
+                    success = true;
+                }
+            }
+            catch
+            {
+                success = false;
+            }
+
+            // Write to the file the CourseNo and CourseName
+            // Write the name of all the students in the course
+            return success;
+        }
+        public bool LoadFromFile(string filePath)
+        {
+            bool success = false;
+
+            try
+            {
+                using(StreamReader reader = new StreamReader(filePath))
+                {
+                    // Read the CourseNo and Coursename then all the students in the course
+                    var CourseNo = reader.ReadLine();
+                    var CourseName = reader.ReadLine();
+                    // Read the student names from the file
+                    while (reader.EndOfStream == false)
+                    {
+                        string? lineData = reader.ReadLine();
+                        if (!string.IsNullOrEmpty(lineData))
+                        {
+                            Students.Add(lineData);
+                        }
+                    }
+                }
+                success = true;
+            }
+            catch
+            {
+                success=false;
+            }
+            return success;
+        }
+        #endregion
+    }
+}
--- OOPDemo01/OOPDemo01/Program.cs
+++ OOPDemo01/OOPDemo01/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using OOPDemo01;
+using static System.Console; // Allows you to 
+
+
+// Create new Course Object
+Course cpsc1517Course = new Course("CPSC1517", "Introduction to Application Development");
+//var cpsc1517Course = new Course("CPSC1517", "Introduction to Application Development");
+//Course cpsc1517Course = new ("CPSC1517", "Introduction to Application Development");
+WriteLine($"CourseNo: {cpsc1517Course.CourseNo}");
+WriteLine($"CourseName: {cpsc1517Course.CourseName}");
+
+// add some students to the course
+//cpsc1517Course.AddStudent("Aaron Fong");
+//cpsc1517Course.AddStudent("David L. Mckinley");
+//cpsc1517Course.AddStudent("Hamza Said");
+//cpsc1517Course.AddStudent("Haseeb Memon");
+//cpsc1517Course.AddStudent("Allaine Paredes");
+cpsc1517Course.LoadFromFile(@"C:\Users\Admin\Desktop\CPSC1517/Students.txt");
+
+// Display all the student in the course
+foreach (var currentStudent in cpsc1517Course.Students)
+{
+    WriteLine(currentStudent);
+}
+
+// Remove 2 student from the course
+cpsc1517Course.RemoveStudent("Hamza Said");
+cpsc1517Course.RemoveStudent("Haseeb Memon");
+
+// Display the number of students
+WriteLine($"There are now {cpsc1517Course.StudentCount} students.");
