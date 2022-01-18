fff5116321fdf0b2912f4c5ee26668ed7857122b
Allaine Paredes <aparedes2@nait.ca>
2022-01-18 14:28:00 -0700

    struct demo

---
+++ StructDemo01/StructDemo01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StructDemo01;
+
+Resolution hdResolution = new(1920, 1080);
+var cinemaSolution = hdResolution;
+cinemaSolution.Width = 1048;
+Console.WriteLine($"HD resolution is {hdResolution.Width} pixels to {hdResolution.Height} pixels height");
+Console.WriteLine($"Cinema resolution is {cinemaSolution.Width} pixels to {cinemaSolution.Height} pixels height");
+
+
+
+//VideoMode someVideoMode = new();
